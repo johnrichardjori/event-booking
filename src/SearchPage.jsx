71f3a30a0a0130d073d364a8bede1b5c215e5b6b
johnrichardjori <johnrichardjori123@gmail.com>
2025-07-25 17:48:21 +0530

    changed

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -46,28 +46,30 @@ export default function SearchPage() {
         }}
       >
         <div id="state">
-          <select value={state} onChange={(e) => setState(e.target.value)}>
-            <option value="">Select state</option>
+          <ul>
             {states.map((s) => (
-              <option key={s} value={s}>
+              <li
+                key={s}
+                onClick={() => {
+                  setState(s);
+                  setCity("");
+                }}
+              >
                 {s}
-              </option>
-            ))}
-          </select>
+              </li>
+            ))}{" "}
+          </ul>
         </div>
         <div id="city">
-          <select
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            disabled={!state}
-          >
-            <option value="">Select city</option>
-            {cities.map((c) => (
-              <option key={c} value={c}>
-                {c}
-              </option>
-            ))}
-          </select>
+          {state && (
+            <ul>
+              {cities.map((c) => (
+                <li key={c} onClick={() => setCity(c)}>
+                  {c}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         <button type="submit" id="searchBtn">
           Search
